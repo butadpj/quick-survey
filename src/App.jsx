ba71879255a5a906c903aa4daa9026a5223c6a51
butadpj <butadpj@gmail.com>
2024-10-24 22:46:52 +0800

    Edit styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import { ref as firebaseRef, onValue, update } from "firebase/database";
 import { database } from "./utils/firebase";
 
